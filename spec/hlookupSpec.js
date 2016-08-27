d401fe2fa23d689371cd7a07c72f54525920a13a
Rajiv Narayana <rajiv@webileapps.com>
2016-08-27 23:59:28 +0530

    Added H and V Lookup tests.

---
+++ spec/hlookupSpec.js
@@ -0,0 +1,28 @@
+var workbook = require('../lib/workbook');
+
+describe("V and H LOOKUP tests ", () => {
+    var wb = new workbook();
+    it ("should hLookup ", ()=> {
+        let set = (ref, val) => wb.set(sheet, ref, val)
+        var sheet = wb.sheet();
+        const rows = 5;
+        let counter = 1;
+        ["Sunday","Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"].forEach((day, index)=>{
+            set(`${String.fromCharCode(65+index)}1`, day);
+            new Array(rows).fill(0).forEach((val, colIndex) => set(`${String.fromCharCode(65+index)}${colIndex+2}`, rows * index + colIndex+ 1))
+        });
+        expect(+wb.run(sheet, `=HLOOKUP("Tuesday",A1:G6,3,0)`)).toBe(12)
+    })
+
+    it ("should vLookup ", ()=> {
+        let set = (ref, val) => wb.set(sheet, ref, val)
+        var sheet = wb.sheet();
+        const rows = 5;
+        let counter = 1;
+        ["Sunday","Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"].forEach((day, index)=>{
+            set(`A${index+1}`, day);
+            new Array(rows).fill(0).forEach((val, colIndex) => set(`${String.fromCharCode(65+colIndex+1)}${index+1}`, rows * index + colIndex+ 1))
+        });
+        expect(+wb.run(sheet, `=VLOOKUP("Tuesday",A1:E7,3,0)`)).toBe(12)
+    })
+});
